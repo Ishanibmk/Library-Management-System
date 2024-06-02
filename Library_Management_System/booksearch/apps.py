4273c7e90b4d1f1a99d9da33fe7f6b7afacdb476
Ishanibmk <155842513+Ishanibmk@users.noreply.github.com>
2024-06-02 22:05:58 +0530

    Project update

--- Library_Management_System/booksearch/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooksearchConfig(AppConfig):
-    name = 'booksearch'
--- Library_Management_System/payfine/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Book)
-admin.site.register(models.Authors)
-admin.site.register(models.BookAuthors)
-admin.site.register(models.BookLoans)
-admin.site.register(models.Borrower)
-admin.site.register(models.Fines)
